ad1b8c3e6dd2bc73a2d84cf55fb68793e8c794de
erik-lance <eriklance@gmail.com>
2023-10-03 19:24:20 +0800

    Added recyclerview functionality

--- app/src/main/java/com/mobdeve/s13/tiongquico/erik/exercise2/MainActivity.kt
+++ app/src/main/java/com/mobdeve/s13/tiongquico/erik/exercise2/MainActivity.kt
@@ -2,10 +2,44 @@ package com.mobdeve.s13.tiongquico.erik.exercise2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.PagerSnapHelper
+import androidx.recyclerview.widget.SnapHelper
+import com.mobdeve.s13.tiongquico.erik.exercise2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        // Perform View Binding
+        val viewBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
+
+        // Anime Titles
+        viewBinding.titlesRv.adapter = AnimeAdapter(DataGenerator.generateAnimeTitleData())
+
+        val animeLinearLayoutManager = LinearLayoutManager(this)
+        animeLinearLayoutManager.orientation = LinearLayoutManager.HORIZONTAL
+
+        viewBinding.titlesRv.layoutManager = animeLinearLayoutManager
+
+        val animeSnapHelper: SnapHelper = PagerSnapHelper()
+        animeSnapHelper.attachToRecyclerView(viewBinding.titlesRv)
+
+        // Reviews
+
+        // Set the adapter (logic has been compressed into one line)
+        viewBinding.episodesRv.adapter = ReviewAdapter(DataGenerator.generateRecentReviewsData())
+
+        // Create a LinearLayoutManager with a horizontal orientation
+        val episodesLinearLayoutManager = LinearLayoutManager(this)
+        episodesLinearLayoutManager.orientation = LinearLayoutManager.VERTICAL
+
+        // Set the LayoutManager
+        viewBinding.episodesRv.layoutManager = episodesLinearLayoutManager
+
+        val episodesSnapHelper: SnapHelper = PagerSnapHelper()
+        episodesSnapHelper.attachToRecyclerView(viewBinding.episodesRv)
+
     }
 }
