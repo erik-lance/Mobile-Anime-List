2d6f9a1cd25ec6c5f2fd6ea16a67eb47c397f73b
erik-lance <eriklance@gmail.com>
2023-10-03 19:09:27 +0800

    Added review recycler view logic

--- app/src/main/java/com/mobdeve/s13/tiongquico/erik/exercise2/ReviewAdapter.kt
+++ app/src/main/java/com/mobdeve/s13/tiongquico/erik/exercise2/ReviewAdapter.kt
@@ -1,19 +1,25 @@
 package com.mobdeve.s13.tiongquico.erik.exercise2
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.mobdeve.s13.tiongquico.erik.exercise2.databinding.ReviewItemLayoutBinding
 
 class ReviewAdapter(private val data: ArrayList<Review>) : RecyclerView.Adapter<ReviewViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ReviewViewHolder {
-        TODO("Not yet implemented")
+        val itemViewBinding: ReviewItemLayoutBinding = ReviewItemLayoutBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false)
+        return ReviewViewHolder(itemViewBinding)
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return this.data.size
     }
 
     override fun onBindViewHolder(holder: ReviewViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.bindData(this.data[position])
     }
 
 }
--- app/src/main/java/com/mobdeve/s13/tiongquico/erik/exercise2/ReviewViewHolder.kt
+++ app/src/main/java/com/mobdeve/s13/tiongquico/erik/exercise2/ReviewViewHolder.kt
@@ -5,6 +5,10 @@ import com.mobdeve.s13.tiongquico.erik.exercise2.databinding.ReviewItemLayoutBin
 
 class ReviewViewHolder(private val viewBinding: ReviewItemLayoutBinding): RecyclerView.ViewHolder(viewBinding.root) {
     fun bindData(model: Review) {
-        TODO("Not yet implemented")
+        this.viewBinding.reviewAuthorTv.text = model.username
+        this.viewBinding.reviewScoreTv.text = model.rating.toString()
+        this.viewBinding.reviewEpisodeTv.text = model.episode.title
+        this.viewBinding.reviewIv.setImageResource(model.episode.anime.imageId)
+        this.viewBinding.reviewTitleTv.text = model.episode.anime.name
     }
 }
