48983d8a86aada37c818324cc8f89f465bb67e23
erik-lance <eriklance@gmail.com>
2023-10-03 18:30:14 +0800

    Added initial adapter and viewholder for reviews

---
+++ app/src/main/java/com/mobdeve/s13/tiongquico/erik/exercise2/ReviewAdapter.kt
@@ -0,0 +1,19 @@
+package com.mobdeve.s13.tiongquico.erik.exercise2
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class ReviewAdapter(private val data: ArrayList<Review>) : RecyclerView.Adapter<ReviewViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ReviewViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ReviewViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+}
---
+++ app/src/main/java/com/mobdeve/s13/tiongquico/erik/exercise2/ReviewViewHolder.kt
@@ -0,0 +1,10 @@
+package com.mobdeve.s13.tiongquico.erik.exercise2
+
+import androidx.recyclerview.widget.RecyclerView
+import com.mobdeve.s13.tiongquico.erik.exercise2.databinding.ReviewItemLayoutBinding
+
+class ReviewViewHolder(private val viewBinding: ReviewItemLayoutBinding): RecyclerView.ViewHolder(viewBinding.root) {
+    fun bindData(model: Review) {
+        TODO("Not yet implemented")
+    }
+}
