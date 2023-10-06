d5c8840afe81eee7aa979c3a6cf828bb5c174031
erik-lance <eriklance@gmail.com>
2023-10-06 20:29:25 +0800

    Adjusted score to include the maximum score and fixed author spacing

--- app/src/main/java/com/mobdeve/s13/tiongquico/erik/exercise2/ReviewViewHolder.kt
+++ app/src/main/java/com/mobdeve/s13/tiongquico/erik/exercise2/ReviewViewHolder.kt
@@ -5,8 +5,10 @@ import com.mobdeve.s13.tiongquico.erik.exercise2.databinding.ReviewItemLayoutBin
 
 class ReviewViewHolder(private val viewBinding: ReviewItemLayoutBinding): RecyclerView.ViewHolder(viewBinding.root) {
     fun bindData(model: Review) {
-        this.viewBinding.reviewAuthorTv.text = model.username
-        this.viewBinding.reviewScoreTv.text = model.rating.toString()
+        val author = " " + model.username
+        val score = model.rating.toString() + "/5.0"
+        this.viewBinding.reviewAuthorTv.text = author
+        this.viewBinding.reviewScoreTv.text = score
         this.viewBinding.reviewEpisodeTv.text = model.episode.title
         this.viewBinding.reviewIv.setImageResource(model.episode.anime.imageId)
         this.viewBinding.reviewTitleTv.text = model.episode.anime.name
